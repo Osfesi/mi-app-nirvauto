126e2306d753d17ffcc35308bdb7a656bf9a72c0
Osfesi <osfesi@gmail.com>
2025-08-27 19:20:23 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3000; // Esta línea no es necesaria para Vercel
 
 // Middleware para procesar JSON
 app.use(express.json());
